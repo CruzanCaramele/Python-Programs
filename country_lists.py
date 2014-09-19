1e21b8373819497c182bdcb76629c9855df324ed
Hamza <letters2hamza@gmail.com>
2014-09-19 19:01:43 +0800

    Create country_lists.py
    lists calculations

---
+++ country_lists.py
@@ -0,0 +1,22 @@
+# Given the variable countries defined as:
+
+
+#             Name    Capital  Populations (millions)
+countries = [['China','Beijing',1350],
+             ['India','Delhi',1210],
+             ['Romania','Bucharest',21],
+             ['United States','Washington',307]]
+
+# Write code to print out the capital of India
+# by accessing the array.
+
+
+print countries[1][1]
+
+
+
+# What multiple of Romania's population is the population
+# of China? Please print your result.
+
+
+print countries[0][2] / countries [2][2]
