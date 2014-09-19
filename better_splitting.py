f50e6be39447c0abe4aeda028360ba7b5d0183da
Hamza <letters2hamza@gmail.com>
2014-09-19 18:59:32 +0800

    Create better_splitting.py
    this program splits a sentence while handling punctuations

---
+++ better_splitting.py
@@ -0,0 +1,29 @@
+def split_string(source, splitlist):
+    output = []
+    atsplit = True#At split point
+    for char in source:
+        if char in splitlist:
+            atsplit = True
+        else:
+            if atsplit:
+                output.append(char)
+                atsplit = False
+            else:
+                output[-1] = output[-1] + char
+    return output
+
+
+
+
+out = split_string("This is a test-of the,string separation-code!"," ,!-")
+print out
+#>>> ['This', 'is', 'a', 'test', 'of', 'the', 'string', 'separation', 'code']
+
+out = split_string("After  the flood   ...  all the colors came out.", " .")
+print out
+#>>> ['After', 'the', 'flood', 'all', 'the', 'colors', 'came', 'out']
+
+out = split_string("First Name,Last Name,Street Address,City,State,Zip Code",",")
+print out
+#>>>['First Name', 'Last Name', 'Street Address', 'City', 'State', 'Zip Code']
+            
